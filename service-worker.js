b757c637466fd13fdc5b75487e649c59f9eebbf3
drmwryd <92357380+drmwryd@users.noreply.github.com>
2023-10-11 20:17:43 +0700

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+
+// Instalasi Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('KING-COFFE').then(function(cache) {
+      return cache.addAll([
+        'http://localhost/king-coffe ',
+        // Tambahkan sumber daya lain yang ingin Anda cache di sini
+      ]);
+    })
+  );
+});
+
+// Aktivasi Service Worker
+self.addEventListener('activate', function(event) {
+  // Tambahkan logika aktivasi di sini
+});
+
+// Intersepsi permintaan fetch
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
